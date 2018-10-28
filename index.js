e74ceb11454ce3ecdfb0cb9080d3e6a459d4a30d
Jatin Khattar 2 <jatkhatt0@publicisgroupe.net>
2018-10-28 12:53:38 +0530

    SurveyZapp , Initial Commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+  res.send({ name: "Jatin" });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("SurveyZapp Server Running ...");
+});
